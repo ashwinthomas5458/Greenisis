f94be1d33c32371536777a359d7d23c1b8ca5fc0
Ashwin Thomas <ashwin.thomas.5458@gmail.com>
2020-09-09 17:24:36 +0530

    nav icon and nav bar animated

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ window.onscroll=function(){
         document.getElementById("logo").style.display="flex"
     }
 }
+const navSlide=()=>{
+    const icon=document.querySelector('.navicon')
+    const nav=document.querySelector('.nav')
+    const bar=document.querySelector('.navlist')
+    nav.addEventListener('click', ()=> {
+        bar.classList.toggle('nav-active')
+        icon.classList.toggle('x')
+    })
+}
+navSlide();
