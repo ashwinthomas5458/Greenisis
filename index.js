ca3953368cd404f43bda9a8a9160b57af001fb44
Ashwin Thomas <ashwin.thomas.5458@gmail.com>
2020-09-10 09:58:25 +0530

    images and layout changed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var abt=document.getElementById("banner").offsetTop
 window.onscroll=function(){
     if(window.pageYOffset<abt){
-        document.getElementById("header").style.backgroundColor="#01475800"
+        document.getElementById("header").style.backgroundColor="#09191d00"
         document.getElementById("logo").style.display="none"
     }
     if(window.pageYOffset>abt){
-        document.getElementById("header").style.backgroundColor="#014758"
+        document.getElementById("header").style.backgroundColor="#09191d"
         document.getElementById("logo").style.display="flex"
     }
 }
@@ -19,3 +19,30 @@ const navSlide=()=>{
     })
 }
 navSlide();
+
+function step(n){
+    var x=document.getElementsByClassName("pic")
+    for(i=0;i<6;i++){
+        if(x[i].style.display!="none"){
+            var f=i
+            break
+        }
+    }
+    if(f==0&n==-1){
+        var k=5
+    }
+    else if(f==5&n==1){
+        k=0
+    }
+    else{
+        k=f+n;
+    }
+    for(j=0;j<6;j++){
+        if(k!=j){
+            x[j].style.display="none"
+        }
+        else{
+            x[j].style.display="block"
+        }
+    }
+}
