a36f6d7f63d1c94be2ac63ee8abdce0f2977161a
Ashwin Thomas <ashwin.thomas.5458@gmail.com>
2020-09-16 13:49:04 +0530

    form rerouting

--- index.js
+++ index.js
@@ -46,3 +46,61 @@ function step(n){
         }
     }
 }
+var submitted=false
+function validateName() {
+    var name = document.getElementById('name').value;
+    if(name.length == 0) {
+      alert("Name can't be blank") ;
+      return false;
+
+    }
+    if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+      alert("Please enter your correct name") ;//Validation Message
+      return false;
+    }
+    return true;
+  }
+
+  function validatePhone() {
+    var phone = document.getElementById('phone').value;
+    if(phone.length == 0) {
+      alert("Phone number can't be blank") ;//Validation Message
+      return false;
+    }
+
+   return true;
+
+ }
+
+ function validateEmail () {
+
+  var email = document.getElementById('email').value;
+  if(email.length == 0) {
+    alert("Email can't be blank") ;//Validation Message
+    return false;
+
+  }
+
+
+  return true;
+
+}
+
+
+function validateForm() {
+  if (!validateName() || !validatePhone() || !validateEmail()) {
+
+    alert("Form not submitted");//Validation Message
+    return false;
+  }
+  else {
+    submitted=true;
+    return true;
+  }
+
+}
+function reloadp(){
+    if(submitted){
+        window.location="index.html"
+    }
+}
